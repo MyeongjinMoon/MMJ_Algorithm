6d718613f3866fabe75374b11701ad1f8c2934aa
MyeongjinMoon <114853338+MyeongjinMoon@users.noreply.github.com>
2023-10-19 18:21:40 +0900

    [Silver III] Title: 파도반 수열, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+#pragma warning(disable:4996)
+
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
+
+int main() {
+	int n;
+	scanf("%d", &n);
+
+	long long* answer = (long long*)malloc(sizeof(long long) * n);
+
+	for (int i = 0; i < n; i++) {
+		int value;
+		scanf("%d", &value);
+
+		unsigned long long* seq = (unsigned long long*)malloc(sizeof(unsigned long long) * value);
+
+		for (int i = 0; i < MIN(5, value); i++) {
+			if (i > 2) {
+				seq[i] = 2;
+				continue;
+			}
+			seq[i] = 1;
+		}
+
+		for (int i = 5; i < value; i++) {
+			seq[i] = seq[i - 1] + seq[i - 5];
+		}
+
+		answer[i] = seq[value - 1];
+	}
+
+	for (int i = 0; i < n; i++) {
+		printf("%lld\n", answer[i]);
+	}
+}
