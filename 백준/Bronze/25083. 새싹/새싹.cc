8da854d5b3a44bada42b4a6fcea5c478854b4f2b
MyeongjinMoon <114853338+MyeongjinMoon@users.noreply.github.com>
2023-11-07 16:29:51 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+#pragma warning(disable:4996)
+
+int main() {
+	printf("         ,r'\"7\n");
+	printf("r`-_   ,'  ,/\n");
+	printf(" \\. \". L_r'\n");
+	printf("   `~\\/\n");
+	printf("      |\n");
+	printf("      |");
+}
