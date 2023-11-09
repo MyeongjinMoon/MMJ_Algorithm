eebfa57b7afe89ce425a303a7168a13fb8097130
MyeongjinMoon <114853338+MyeongjinMoon@users.noreply.github.com>
2023-11-09 13:24:51 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int n,m;
+    
+    scanf("%d %d", &n, &m);
+    
+    int** a = (int**)malloc(sizeof(int*) * n);
+    int** b = (int**)malloc(sizeof(int*) * n);
+    for(int i=0;i < n;i++){
+        a[i] = (int*)malloc(sizeof(int)*m);
+        b[i] = (int*)malloc(sizeof(int)*m);
+    }
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            scanf("%d ", &a[i][j]);
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            scanf("%d ", &b[i][j]);
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            printf("%d ", a[i][j] + b[i][j]);
+        }
+        printf("\n");
+    }
+}
