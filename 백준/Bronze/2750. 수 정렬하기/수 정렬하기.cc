cc40388e2abc8bbd3c3a555a6d3f65022fbebd1a
MyeongjinMoon <114853338+MyeongjinMoon@users.noreply.github.com>
2023-11-15 12:07:57 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+#pragma warning(disable:4996)
+
+int main() {
+	int a[1001];
+	int n;
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
+	}
+
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (a[i] > a[j]) {
+				int temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		printf("%d\n", a[i]);
+	}
+}
