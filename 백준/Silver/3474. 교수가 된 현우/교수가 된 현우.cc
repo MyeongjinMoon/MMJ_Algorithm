f3c3727f2efcd0bf7db4fe9cbd931d013fd39342
MyeongjinMoon <114853338+MyeongjinMoon@users.noreply.github.com>
2024-02-28 17:31:41 +0900

    [Silver III] Title: 교수가 된 현우, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <map>
+
+/*
+따닥따닥 붙어있을 땐 scanf("1%d")로 하나씩 받기
+*/
+
+/*
+문제가 분할정복일 경우 재귀 or Stack으로 풀기.
+행위 반복, 매개변수는 같음 = 재귀함수 사용
+string(1,a[y][x]); * 1은 문자열 길이 a[y][x]는 변환할 문자
+*/
+
+/*
+문자열 custom_operator
+ - 문자열은 앞에서부터 아스키코드 값으로 비교하기 때문에 sort할 때 custom_operator
+ 를 사용해야 함.
+
+bool cmp(string a, string b) {
+    if (a.size() == b.size())   //2순위 사이즈 같을경우 내용을 비교
+        return a < b;
+    return a.size() < b.size(); //1순위 사이즈 비교
+}
+*/
+
+using namespace std;
+const int max_n = 101;
+int dy[4] = { -1, 0, 1, 0 };
+int dx[4] = { 0, -1, 0, 1 };
+//int a[max_n][max_n], visited[max_n][max_n];
+int a, m, n, j, k, y, x, ny, nx, t, c;
+int x1, x2, y_1, y2;
+string s, ret;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        int ret5 = 0;
+        for (int j = 5; j <= a; j *= 5) {
+            ret5 += a / j;
+        }
+        cout << ret5 << "\n";
+    }
+
+    return 0;
+}
