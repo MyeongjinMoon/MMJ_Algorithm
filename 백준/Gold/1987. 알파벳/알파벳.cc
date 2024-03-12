aea881fc0f753e6a592d493068bc5a114310a25a
MyeongjinMoon <114853338+MyeongjinMoon@users.noreply.github.com>
2024-03-12 23:23:42 +0900

    [Gold IV] Title: 알파벳, Time: 400 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,94 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<iostream>
 
-#pragma warning(disable:4996)
+using namespace std;
 
-#define MAX 401
+int R, C, ret, cnt;
+char a[21][21];
+int visited[26];
+int dy[4] = { -1,0,1,0 }, dx[4] = { 0,-1,0,1 };
 
-int dx[4] = { 1,0,-1,0 };
-int dy[4] = { 0,1,0,-1 };
-
-typedef struct Point {
-	int x, y;
-	int dis;
-}Point;
-
-typedef struct Stack {
-	Point* arr;
-	int tail;
-}Stack;
-
-void initStack(Stack* s) {
-	s->arr = (Point*)malloc(sizeof(Point) * MAX);
-	s->tail = 0;
-}
-
-bool isFull(Stack* s) {
-	return (s->tail == MAX);
-}
-
-bool isEmpty(Stack* s) {
-	return (s->tail == 0);
-}
-
-void push(Stack* s, Point value) {
-	s->arr[s->tail++] = value;
-}
-
-Point pull(Stack* s) {
-	if (s->tail == 0) {
-		return {0, 0, 0};
+void go(int y, int x, int cnt) {
+	ret = max(ret, cnt);
+	for (int i = 0; i < 4; i++) {
+		int ny = y + dy[i], nx = x + dx[i];
+		if (ny < 0 || ny >= R || nx < 0 || nx >= C)continue;
+		if (visited[(int)a[ny][nx] - 'A'])continue;
+		visited[(int)a[ny][nx] - 'A'] = 1;
+		go(ny, nx, cnt + 1);
+		visited[(int)a[ny][nx] - 'A'] = 0;
 	}
-	return s->arr[--s->tail];
-}
-
-bool isvisited(char* flag, char value, int dis) {
-	for (int i = 1; i <= dis; i++) {
-		if (value == flag[i])
-			return true;
-	}
-	return false;
+	return;
 }
 
 int main() {
-	int R, C;
+	cin >> R >> C;
 
-	scanf("%d %d", &R, &C);
-	char** mtrx = (char**)malloc(sizeof(char*) * R);
 	for (int i = 0; i < R; i++) {
-		mtrx[i] = (char*)malloc(sizeof(char) * C);
 		for (int j = 0; j < C; j++) {
-			scanf(" %c", &mtrx[i][j]);
+			cin >> a[i][j];
 		}
 	}
-
-	char* flag = (char*)malloc(sizeof(char) * MAX);
-
-	Stack s;
-	int max = 1;
-	initStack(&s);
-
-	push(&s, { 0, 0, 1});
-
-
-	while(!isEmpty(&s)){
-		Point current = pull(&s);
-
-		if (current.dis > max)
-			max = current.dis;
-		
-		int x = current.x, y = current.y;
-		flag[current.dis] = mtrx[x][y];
-
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i], ny = y + dy[i];
-
-			if (nx >= 0 && ny >= 0 && nx < R && ny < C && !isvisited(flag, mtrx[nx][ny], current.dis)) {
-				push(&s, { nx, ny, current.dis + 1 });
-			}
-		}
-		
-	}
-
-	printf("%d", max);
+	visited[(int)a[0][0] - 'A'] = 1;
+	go(0, 0, 1);
+	cout << ret;
 }
