e49246f69571309a4741ac1c82922fa094a3a12f
MyeongjinMoon <114853338+MyeongjinMoon@users.noreply.github.com>
2024-04-10 16:15:26 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.cc
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -1,30 +1,31 @@
-#include <iostream>
-#include <istream>
-#include <vector>
-#include <algorithm>
-#include <string.h>
+#include<iostream>
+#include<string>
 
 using namespace std;
 
+string s;
+
 int main() {
-    string a;
-    getline(cin, a, '\n');
-    for(int i = 0 ; i < a.size();i++){
-        if(a[i] >= 65 && a[i] <= 90){
-            a[i] += 13;
-            if(a[i] > 90){
-                a[i] -= 26;
-            }
-        }
-        else if(a[i] >= 97 && a[i] <= 122){
-            int c = a[i] + 13;
-            if(c > 122){
-                a[i] = c - 26;
-            }
-            else{
-                a[i] = c;
-            }
-        }
-    }
-    cout << a;
+	getline(cin, s);
+
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] >= 'A' && s[i] <= 'Z') {
+			s[i] += 13;
+			if (s[i] > 'Z') {
+				s[i] -= 26;
+			}
+		}
+		else if (s[i] >= 'a' && s[i] <= 'z') {
+			if (s[i] + 13 > 'z') {
+				s[i] -= 13;
+			}
+			else {
+				s[i] += 13;
+			}
+		}
+	}
+
+	cout << s;
+
+	return 0;
 }
